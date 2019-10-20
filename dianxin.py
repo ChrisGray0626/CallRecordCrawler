bda5f91392623c0448374bf594ab08f1897656db
ChrisGray822626 <41218402+ChrisGray822626@users.noreply.github.com>
2019-10-20 22:37:51 +0800

    up

--- dianxin.py
+++ dianxin.py
@@ -1,51 +1,45 @@
 from selenium import webdriver
+import lxml
 from lxml import etree
 from time import sleep
 import PIL.Image as Image
 import json
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-
-def main(phoneNum, serverCode, idNum, name):
+def main(phoneNum,password,idNum,name):
     # 初始化
     loginUrl = "http://login.189.cn/web/login"
-    driver = webdriver.Chrome()
+
+    options = Options()
+    options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=options)
+    wait = WebDriverWait(driver, 10)
     driver.get(loginUrl)
 
     # 输入账号
-    AccountInput = driver.find_element_by_name("Account")
-    AccountInput.send_keys(phoneNum)
-    AccountInput.click()
+    accountInput = driver.find_element_by_name("Account")
+    accountInput.send_keys(phoneNum)
+    accountInput.click()
+
     sleep(0.5)
 
     # 密码
-    PwdInput = driver.find_element_by_id("txtShowPwd")
-    PwdInput.click()
+    pwdInput = driver.find_element_by_id("txtShowPwd")
+    pwdInput.click()
 
     # 输入框发生了变化
-    PwdInput = driver.find_element_by_name("Password")
-    PwdInput.send_keys(serverCode)
-    sleep(1)
+    pwdInput = driver.find_element_by_name("Password")
+    pwdInput.send_keys(password)
 
     # 保存验证码
     driver.get_screenshot_as_file("screen.png")
     body = driver.find_element_by_xpath("//body[1]")
     caption = driver.find_element_by_id("imgCaptcha")
-    left = int(caption.location['x'])
-    top = int(caption.location['y'])
-    bottom = int(caption.location['y'] + caption.size['height'])
-    originHeight = body.size['height']
-    originWidth = body.size['width']
-    im = Image.open('screen.png')
-    picHeight = im.size[1]
-    picWidth = im.size[0]
-
-    left = int(left*picWidth/originWidth)
-    right = left + caption.size['width']
-    top = int(top*picHeight/originHeight)
-    bottom = int(bottom*picHeight/originHeight)
-
-    img = im.crop((left,top,right,bottom))
-    img.save('code.png')
+    caption.screenshot("code.png")
 
     # 读取验证码
     Captcha = driver.find_element_by_name("Captcha")
@@ -56,16 +50,19 @@ def main(phoneNum, serverCode, idNum, name):
     # 登录
     loginBtn = driver.find_element_by_id("loginbtn")
     loginBtn.click()
-    sleep(1.5)
+    wait.until(EC.presence_of_element_located((By.XPATH,"//a[@class='headmain_bg']")))
+
+
 
     # 开始请求
     detailUrl = "https://www.189.cn/dqmh/ssoLink.do?method=linkTo&platNo=10012&toStUrl=https://zj.189.cn/service/queryorder/"
     driver.get(detailUrl)
-    sleep(2)
+
+    wait.until(EC.presence_of_element_located((By.XPATH,"//img[@class='dpb mb5']")))
 
     queryUrl = "http://zj.189.cn//zjpr/service/query/query_order.html?menuFlag=1"
     driver.get(queryUrl)
-    sleep(2.5)
+    wait.until(EC.presence_of_element_located((By.XPATH,"//a[@id='codekey']")))
 
     nameBox = driver.find_element_by_name("cdrCondition.usernameyanzheng")
     idBox = driver.find_element_by_name("cdrCondition.idyanzheng")
@@ -90,10 +87,10 @@ def main(phoneNum, serverCode, idNum, name):
             month.click()
 
 
-        print("月份:", title)
+        print("月份:",title)
         if i == 1:
-            codeKey = driver.find_element_by_id("codekey")
-            codeKey.click()
+            codekey = driver.find_element_by_id("codekey")
+            codekey.click()
             inputCodeKey = driver.find_element_by_name("cdrCondition.randpsw")
             key = input("请输入短信验证码: ")
             inputCodeKey.send_keys(key)
@@ -108,8 +105,11 @@ def main(phoneNum, serverCode, idNum, name):
         sleep(0.5)
 
         driver.switch_to.window(driver.window_handles[1])
-        sleep(3)
-        source = driver.page_source
+        wait.until(EC.presence_of_element_located((By.XPATH,"//input[@name='button']")))
+        try:
+            source = driver.page_source
+        except:
+            source = ""
 
         getRecord(source, title)
         driver.close()
@@ -120,12 +120,12 @@ def main(phoneNum, serverCode, idNum, name):
 def getRecord(source, title):
     if "您没有该项清单" in source:
         print("您没有该项清单")
-        save("", title)
+        save("",title)
         return
     html = etree.HTML(source)
 
     data = []
-    for i in range(3, 100):
+    for i in range(3,100):
         context = html.xpath('//table[@class="cdrtable"][1]/tbody/tr[{}]/td/text()'.format(i))
         if len(context) == 0:
             break
@@ -143,12 +143,12 @@ def getRecord(source, title):
             "对方号码": number,
             "呼叫类型": phoneType,
             "通话起始时间": time,
-            "通话时长": length,
+            "通话时长":length,
             "通话地": location,
-            "通话类型": callType,
-            "通话费": fee,
-            "减免": reduce,
-            "费用小计": all,
+            "通话类型":callType,
+            "通话费":fee,
+            "减免":reduce,
+            "费用小计":all,
         }
 
         data.append(dic)
@@ -159,13 +159,15 @@ def getRecord(source, title):
 def save(data, title):
     path = title+".json"
     data = json.dumps(data, ensure_ascii=False)
-    with open(path, "a", encoding="utf-8") as f:
+    with open(path,"a",encoding="utf-8") as f:
         f.write(data)
 
 
 if __name__ == '__main__':
-    phoneNum = input("请输入您的手机号：")
-    serverCode = input("请输入您的服务密码：")
-    idNum = input("请输入您的身份证号：")
-    name = input("请输入您的姓名：")
-    main(phoneNum, serverCode, idNum, name)
+    #依次填入手机号，服务密码，身份证号，姓名
+    phoneNum = input("请输入手机号: ")
+    password = input("请输入服务密码: ")
+    idNum = input("请输入身份证号: ")
+    name = input("请输入姓名: ")
+
+    main(phoneNum, password, idNum, name)
